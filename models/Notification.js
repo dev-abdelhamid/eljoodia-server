d20f7e50bec9f9c08b805369fecd9fee4af6f601
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-08-17 15:25:10 +0300

    first commit

--- models/Notification.js
+++ models/Notification.js
@@ -1,15 +1,22 @@
-// models/Notification.js
-const mongoose = require('mongoose');
 const notificationSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   type: {
     type: String,
+    enum: [
+      'order_created',
+      'order_status_updated',
+      'order_delivered',
+      'return_status_updated',
+      'task_assigned', // أضف هذه القيمة
+      'task_status_updated',
+      'task_completed',
+    ],
     required: true,
-    enum: ['order_created', 'order_status_updated', 'return_created', 'return_status_updated', 'order_delivered'],
   },
   message: { type: String, required: true },
-  data: { type: Object, default: {} },
+  data: { type: Object },
   read: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
 });
-module.exports = mongoose.models.Notification || mongoose.model('Notification', notificationSchema);
+
+module.exports = mongoose.model('Notification', notificationSchema);

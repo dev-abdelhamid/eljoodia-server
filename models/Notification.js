e044b5a6bd7fa93935d79ee67cb1b96f39de29e9
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-09-09 01:54:00 +0300

    first

--- models/Notification.js
+++ models/Notification.js
@@ -1,66 +1,121 @@
 const mongoose = require('mongoose');
+
 const { v4: uuidv4 } = require('uuid');
 
 const notificationSchema = new mongoose.Schema({
+
   _id: {
+
     type: String,
+
     default: uuidv4,
+
   },
+
   user: {
+
     type: String,
+
     ref: 'User',
+
     required: true,
+
   },
+
   type: {
+
     type: String,
+
     required: true,
+
     enum: [
+
       'new_order_from_branch',
+
       'branch_confirmed_receipt',
+
       'new_order_for_production',
+
       'order_completed_by_chefs',
+
       'order_approved_for_branch',
+
       'order_in_transit_to_branch',
+
       'new_production_assigned_to_chef',
+
       'order_status_updated',
+
       'task_assigned',
-      'task_completed',
+
       'order_completed',
+
       'order_delivered',
+
       'return_status_updated',
+
       'missing_assignments',
-      'order_created',
+
     ],
+
   },
+
   message: {
+
     type: String,
+
     required: true,
+
   },
+
   data: {
+
     type: mongoose.Schema.Types.Mixed,
+
     default: {},
+
   },
+
   read: {
+
     type: Boolean,
+
     default: false,
+
   },
+
   createdAt: {
+
     type: Date,
+
     default: Date.now,
-    index: { expires: '30d' },
+
+    index: { expires: '30d' }, // حذف الإشعارات بعد 30 يوم
+
   },
+
 }, {
+
   timestamps: true,
+
 });
 
 notificationSchema.pre('save', function(next) {
+
   console.log(`[${new Date().toISOString()}] Pre-save hook for notification:`, {
+
     user: this.user,
+
     type: this.type,
+
     message: this.message,
+
     data: this.data,
+
   });
+
   next();
+
 });
 
 module.exports = mongoose.models.Notification || mongoose.model('Notification', notificationSchema);

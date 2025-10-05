b6c0fa1d0e0b01fafa33431ed1138c9e3b89629a
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-10-06 01:17:54 +0300

    one

--- routes/Inventory.js
+++ routes/Inventory.js
@@ -11,7 +11,7 @@ const {
   getInventoryHistory,
   createInventory,
   bulkCreate,
-} = require('../controllers/inventoryController');
+} = require('../controllers/inventory');
 
 const router = express.Router();
 
